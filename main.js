a45d4c511ec5a0e89fea9999d58a67fedb3453b2
victorcodigos <victor.macedo.dedeus@gmail.com>
2023-06-01 11:22:44 +0200

    contar palabra

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-function contarPalabras (texto = ` ` ) { 
+/* function contarPalabras (texto = ` ` ) { 
     if (typeof texto !== 'string') return 'retornar o valor ingresado como una cadena';
     if (texto === '') return 'Usted no has ingresado ningun valor';
     return texto.length ;
 }
 
 console.log(contarPalabras ('Me gusta Valencia porque es una ciudad que hay playas')); 
+
+*/
+
+
+
+let string = "Buenos dias por la manana";
+let totalPalabras = 0;
+function contarLetras(letra) {
+    let stringMinusculas = string.toLowerCase();
+    for (let i = 0; i < string.length; i++) {
+        if (letra === stringMinusculas[i]) {
+            totalPalabras += 1;
+        }
+    }
+    return totalPalabras;
+}
+
+totalPalabras("z");
